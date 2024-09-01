9c99f99151a02ea706e38032fa03262dc1b41e6f
fabioab1 <fabiobonanatodeabreu990@gmail.com>
2024-09-01 09:51:09 -0300

    Exercício 9 concluído.

---
+++ Lista3/respexer9.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Resposta do Exercício 9</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <main class="container">
+        <h1>Resposta do Exercício 9</h1>
+        <?php
+            if ($_SERVER['REQUEST_METHOD'] == 'POST')
+            {
+                try
+                {
+                    $num = (int) $_POST['num'] ?? 0;
+                    $fatorial = 1;
+
+                    if ($num > 0)
+                        for ($i = 1; $i <= $num; $i++)
+                            $fatorial *= $i;
+
+                    echo "$num! = $fatorial.";
+                }
+                catch (Exception $e)
+                {
+                    echo 'Erro! '.$e->getMessage();
+                }
+            }
+            else
+                echo "<h3>Ops! Algo deu errado...</h3><p>Tente novamente.</p><a class='btn btn-primary' href='exer9.php' role='button'>Voltar</a>";
+
+        ?>
+
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
