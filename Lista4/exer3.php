20c6542c3af72ccecc3dd7739cecd2a576065e3e
fabioab1 <fabiobonanatodeabreu990@gmail.com>
2024-09-04 11:58:42 -0300

    Exercício 3 iniciado.

---
+++ Lista4/exer3.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 3</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <main class="container">
+        <h1>Exercício 3</h1>
+
+        <form action="exer3resp.php" method="POST">
+            <div class="row mb-3">
+                <label for="palavra1" class="col-form-label col-1">Palavra 1:</label>
+                <div class="col-2">
+                    <input type="text" id="palavra1" name="palavra1" class="form-control">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label for="palavra2" class="col-form-label col-1">Palavra 2:</label>
+                <div class="col-2">
+                    <input type="text" id="palavra1" name="palavra1" class="form-control">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-2">
+                    <button type="submit" class="btn btn-primary">Verificar</button>
+                </div>
+            </div>
+        </form>
+
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
